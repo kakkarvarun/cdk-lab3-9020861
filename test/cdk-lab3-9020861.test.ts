3172932413605aabc2d63d69538416510abba566
Varun Kakkar <kakkar.varun67@gmail.com>
2025-11-26 17:47:26 -0500

    chore: initialize CDK TypeScript app for lab 3

---
+++ test/cdk-lab3-9020861.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkLab39020861 from '../lib/cdk-lab3-9020861-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-lab3-9020861-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkLab39020861.CdkLab39020861Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
